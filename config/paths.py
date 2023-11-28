9186cf2bbe08ad187b3ce7ca76c8605bc3f5c136
Dmytro Hakman <201dreamers@gmail.com>
2023-11-28 03:36:46 +0200

    add framework paths into config dir

---
+++ config/paths.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+
+
+class FrameworkPaths:
+    FRAMEWORK_ROOT = Path(os.getenv("FRAMEWORK_DIR", os.getcwd()))
+    CONFIG_DIR = FRAMEWORK_ROOT / "config"
+    DEVICES_YAML = CONFIG_DIR / "devices" / "devices.yaml"
