a6b07818cfe853e024995c59dc2ad0da7251906a
Dmytro Hakman <201dreamers@gmail.com>
2023-11-30 05:02:55 +0200

    add REPORTS_DIR path

--- config/paths.py
+++ config/paths.py
@@ -6,3 +6,4 @@ class FrameworkPaths:
     FRAMEWORK_ROOT = Path(os.getenv("FRAMEWORK_DIR", os.getcwd()))
     CONFIG_DIR = FRAMEWORK_ROOT / "config"
     DEVICES_YAML = CONFIG_DIR / "devices" / "devices.yaml"
+    REPORTS_DIR = FRAMEWORK_ROOT / "reports"
