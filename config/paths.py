971b15d0718bc14257a1f3ec4aa78ba54b766f26
Dmytro Hakman <201dreamers@gmail.com>
2023-12-02 03:45:49 +0200

    move devices.yaml to resources dir

--- config/paths.py
+++ config/paths.py
@@ -5,5 +5,8 @@ from pathlib import Path
 class FrameworkPaths:
     FRAMEWORK_ROOT = Path(os.getenv("FRAMEWORK_DIR", os.getcwd()))
     CONFIG_DIR = FRAMEWORK_ROOT / "config"
-    DEVICES_YAML = CONFIG_DIR / "devices" / "devices.yaml"
+    RESOURCES_DIR = FRAMEWORK_ROOT / "resources"
     REPORTS_DIR = FRAMEWORK_ROOT / "reports"
+
+    DEVICES_YAML = RESOURCES_DIR / "devices.yaml"
+    COMMANDS_YAML = RESOURCES_DIR / "console_commands.yaml"
